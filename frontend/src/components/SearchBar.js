25658bdb3a8d16d3c2c6e449c27861763fa2bf52
JCkwek <kwekjiacong@gmail.com>
2026-03-21 02:45:05 +0800

    FE: included click  enter to search

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,6 +1,11 @@
 import styles from './SearchBar.module.css'
 
 function SearchBar({value, onChange, onSearch}){
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+        onSearch();
+    }
+  };
     return(
         <div className={styles.searchBarContainer}>
             <input type="text" 
@@ -8,6 +13,7 @@ function SearchBar({value, onChange, onSearch}){
             placeholder="Search product"
             value={value}
             onChange={(e)=> onChange(e.target.value)}
+            onKeyDown={handleKeyDown}
             />
                 <button className={styles.searchButton} onClick={onSearch}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
